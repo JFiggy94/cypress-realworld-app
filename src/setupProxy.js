8e17d069c51449d5cbfbd74c96b843f5d15597c7
Jenny Figgy <jennyangelfigueroa@gmail.com>
2021-10-04 16:04:45 -0700

    Hopefully fix startup

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,10 @@
-import { hostName } from "utils/baseUrlUtils"
-
 const createProxyMiddleware = require("http-proxy-middleware");
 require("dotenv").config();
 
 module.exports = function (app) {
   app.use(
     createProxyMiddleware(["/login", "/callback", "/logout", "/checkAuth", "graphql"], {
-      target: `http://${hostName}:${process.env.BACKEND_PORT}`,
+      target: `http://${process.env.REACT_APP_HOSTNAME}:${process.env.BACKEND_PORT}`,
       changeOrigin: true,
       logLevel: "debug",
     })
